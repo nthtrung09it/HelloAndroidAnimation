ce43725b2da590d7b8fa7981ea34f8f18cb7490a
TuanTV <vantuantran@gmail.com>
2017-09-23 15:43:05 +0700

    Add ProgressBar

--- app/src/main/java/nthtrung09it/android/hello/animation/SecondActivity.java
+++ app/src/main/java/nthtrung09it/android/hello/animation/SecondActivity.java
@@ -1,10 +1,16 @@
 package nthtrung09it.android.hello.animation;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author Kernel
@@ -18,9 +24,31 @@ public class SecondActivity extends AppCompatActivity {
         return intent;
     }
 
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(1, 1000);
+        valueAnimator.setDuration(1000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int value = (int) valueAnimator.getAnimatedValue();
+                mProgressBar.setProgress(value);
+            }
+        });
+        valueAnimator.start();
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+        ButterKnife.bind(this);
+
+
     }
 }
